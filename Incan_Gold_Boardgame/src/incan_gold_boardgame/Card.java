655d3cb4085dea4167dae7fcc9fd79bfe09a9c5c
michaeltian9265 <michaeltian9265@gmail.com>
2016-01-11 12:52:49 -0800

    Worked on Card and Deck class
    implemented all the cards as well as initialize and shuffle deck

--- Incan_Gold_Boardgame/src/incan_gold_boardgame/Card.java
+++ Incan_Gold_Boardgame/src/incan_gold_boardgame/Card.java
@@ -11,19 +11,41 @@ package incan_gold_boardgame;
  */
 public class Card {
     
-    String type;
-    int value;
-    String name;
+     String type;
+     int value;
+     String name;
     
     public Card(){
         
     }
-    
-    public void createCard(String type, int value, String name){
+    //Create card object
+    public Card(String type, int value, String name){
         this.type = type;
         this.value = value;
         this.name = name;
         
+    }
+    
+    //getters for card object
+    public String getCardType()
+    {
+        return this.type;
+    }
+    
+    public int getCardValue()
+    {
+        return this.value;
+    }
+    
+    public String getCardName()
+    {
+        return this.name;
+    }
+    
+     @Override
+    public String toString()
+    {
+        return "The card is a" + this.type + ". " + this.name + "with value " + this.value;
     }
     
 }
--- Incan_Gold_Boardgame/src/incan_gold_boardgame/Deck.java
+++ Incan_Gold_Boardgame/src/incan_gold_boardgame/Deck.java
@@ -5,18 +5,95 @@
  */
 package incan_gold_boardgame;
 
+import java.util.Collections;
+import java.util.LinkedList;
 /**
  *
  * @author Michael
  */
 public class Deck {
+    //Variable to store deck
+    LinkedList<Card> myDeck = new LinkedList<>();
+    //Create the 35 unique playing cards in Incan gold
+    Card card1 = new Card("Hazard", 1, "Snake");
+    Card card2 = new Card("Hazard", 2, "Snake");
+    Card card3 = new Card("Hazard", 3, "Snake");
+    Card card4 = new Card("Hazard", 1, "Spider");
+    Card card5 = new Card("Hazard", 2, "Spider");
+    Card card6 = new Card("Hazard", 3, "Spider");
+    Card card7 = new Card("Hazard", 1, "Lava");
+    Card card8 = new Card("Hazard", 2, "Lava");
+    Card card9 = new Card("Hazard", 3, "Lava");
+    Card card10 = new Card("Hazard", 1, "Zombie");
+    Card card11 = new Card("Hazard", 2, "Zombie");
+    Card card12 = new Card("Hazard", 3, "Zombie");
+    Card card13 = new Card("Hazard", 1, "Rockslide");
+    Card card14 = new Card("Hazard", 2, "Rockslide");
+    Card card15 = new Card("Hazard", 3, "Rockslide");
+    Card card16 = new Card("Treasure", 1, "TreasureRoom1");
+    Card card17 = new Card("Treasure", 2, "TreasureRoom2");
+    Card card18 = new Card("Treasure", 3, "TreasureRoom3");
+    Card card19 = new Card("Treasure", 4, "TreasureRoom4");
+    Card card20 = new Card("Treasure", 5, "TreasureRoom5");
+    Card card21 = new Card("Treasure", 6, "TreasureRoom6");
+    Card card22 = new Card("Treasure", 7, "TreasureRoom7");
+    Card card23 = new Card("Treasure", 9, "TreasureRoom9");
+    Card card24 = new Card("Treasure", 10, "TreasureRoom10");
+    Card card25 = new Card("Treasure", 11, "TreasureRoom11");
+    Card card26 = new Card("Treasure", 12, "TreasureRoom12");
+    Card card27 = new Card("Treasure", 13, "TreasureRoom13");
+    Card card28 = new Card("Treasure", 14, "TreasureRoom14");
+    Card card29 = new Card("Treasure", 15, "TreasureRoom15");
+    Card card30 = new Card("Treasure", 17, "TreasureRoom17");
+    Card card31 = new Card("Artifact", 5, "GoldenStatue5");
+    Card card32 = new Card("Artifact", 5, "GoldenRelic5");
+    Card card33 = new Card("Artifact", 5, "GoldenFigure5");
+    Card card34 = new Card("Artifact", 10, "GoldNecklass10");
+    Card card35 = new Card("Artifact", 10, "GoldenCylinder10");
     
+    
+    
+    //Insert the 30 regular playing cards into the deck
     public void initializeDeck(){
         
-        for (int i = 0; i < 30; i++){
-            System.out.println(i);
-        }
+        myDeck.add(card1);
+        myDeck.add(card2);
+        myDeck.add(card3);
+        myDeck.add(card4);
+        myDeck.add(card5);
+        myDeck.add(card6);
+        myDeck.add(card7);
+        myDeck.add(card8);
+        myDeck.add(card9);
+        myDeck.add(card10);
+        myDeck.add(card11);
+        myDeck.add(card12);
+        myDeck.add(card13);
+        myDeck.add(card14);
+        myDeck.add(card15);
+        myDeck.add(card16);
+        myDeck.add(card17);
+        myDeck.add(card18);
+        myDeck.add(card19);
+        myDeck.add(card20);
+        myDeck.add(card21);
+        myDeck.add(card22);
+        myDeck.add(card23);
+        myDeck.add(card24);
+        myDeck.add(card25);
+        myDeck.add(card26);
+        myDeck.add(card27);
+        myDeck.add(card28);
+        myDeck.add(card29);
+        myDeck.add(card30);
         
+              
+        
+        
+    }
+    
+    public void shuffleDeck(){
+        Collections.shuffle(myDeck);
     }
     
 }
